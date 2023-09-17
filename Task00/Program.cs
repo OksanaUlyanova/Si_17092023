4f6f7d056668e1be33282ffa0375f6e621bdc8e6
Oksana_Ulyanova <Ulyanovaoy@mail.ru>
2023-09-17 23:05:55 +0500

    Added Task001

--- Task00/Program.cs
+++ Task00/Program.cs
@@ -11,7 +11,6 @@
 
 Console.WriteLine("Введите целое число:  "); 
 int number = Convert.ToInt32(Console.ReadLine());
-
 int square = number * number;
 Console.WriteLine(square + "->" + square);
-Console.WriteLine($"{number} -> {square})
+//Console.WriteLine($"{number} -> {square});
